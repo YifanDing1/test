bb7565724c59bef8fb26b5d91916e25243503660
YifanDing1 <123893339+YifanDing1@users.noreply.github.com>
2024-06-24 13:51:30 -0500

    Add files via upload

---
+++ IB_sustained_test/dbcre.php
@@ -0,0 +1,7 @@
+<?php
+$servername = "localhost";//"mysql51-004.wc2"; //"72.3.204.190";
+$username = "root";//72.3.204.190 //172.17.66.67; //root user for MySQL database
+$password = "mysql"; //root password
+$dbname = "myDB"; //name assigned upon creation of database;
+$port = 3306;
+?>
